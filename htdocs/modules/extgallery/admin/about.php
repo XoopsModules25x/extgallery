3d1dbb5af4ab045e78b454531d26f48b12b1f3e8
mambax7 <mambax7@gmail.com>
2017-03-29 04:32:24 -0400

    update for PayPal

--- htdocs/modules/extgallery/admin/about.php
+++ htdocs/modules/extgallery/admin/about.php
@@ -21,6 +21,6 @@ include_once __DIR__ . '/admin_header.php';
 xoops_cp_header();
 
 echo $adminObject->addNavigation(basename(__FILE__));
-echo $adminObject->renderAbout('6KJ7RW5DR3VTJ', false);
+echo $adminObject->renderAbout('xoopsfoundation@gmail.com', false);
 
 include_once __DIR__ . '/admin_footer.php';
